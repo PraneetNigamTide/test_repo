847d0d10ea12c9033d2c909a81c9198c0f14791f
PraneetNigamTide <113947844+PraneetNigamTide@users.noreply.github.com>
2023-11-16 04:37:26 +0530

    add upload artifact action

--- src/app.py
+++ src/app.py
@@ -1,4 +1,8 @@
+import logging
 
-
+logging.basicConfig(filename='app.log', filemode='w', format='%(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
 def add(a, b):
+    logger.debug(f"Adding {a} and {b}")
     return a+b
+
