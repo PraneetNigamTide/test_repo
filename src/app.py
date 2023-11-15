43a7a9ecb83e2afbea04a60b434c221138a8af74
PraneetNigamTide <113947844+PraneetNigamTide@users.noreply.github.com>
2023-11-16 04:49:52 +0530

    add downlaod artifact action

--- src/app.py
+++ src/app.py
@@ -4,5 +4,9 @@ logging.basicConfig(filename='app.log', filemode='w', format='%(name)s - %(level
 logger = logging.getLogger(__name__)
 def add(a, b):
     logger.debug(f"Adding {a} and {b}")
+    with open('app_output.txt', mode='w') as file:
+        file.write(f"Adding {a} and {b} return {a+b}")
     return a+b
+
+
 
